bd2e7f75bb219e3118b04b553b3c7875c3856c8b
Randall Degges <rdegges@gmail.com>
2012-12-04 00:08:52 -0800

    Adding a setup.py script.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from os.path import abspath, dirname, join, normpath
+
+from setuptools import setup
+
+
+setup(
+
+    # Basic package information:
+    name = 'data247',
+    version = '0.1',
+    py_modules = ('minify',),
+
+    # Packaging options:
+    zip_safe = False,
+    include_package_data = True,
+
+    # Package dependencies:
+    install_requires = ['django-htmlmin>=0.5.2', 'pelican>=3.1.1'],
+
+    # Metadata for PyPI:
+    author = 'Randall Degges',
+    author_email = 'rdegges@gmail.com',
+    license = 'UNLICENSE',
+    url = 'https://github.com/rdegges/pelican-minify',
+    keywords = 'pelican blog static minify html minification',
+    description = ('An HTML minification plugin for Pelican, the static '
+            'site generator.'),
+    long_description = open(normpath(join(dirname(abspath(__file__)),
+        'README.md'))).read()
+
+)
