71dae4a553e4ffedabac65a1fb7d25962d99c5bf
Alexandre Fonseca <alexandrejorgefonseca@gmail.com>
2014-08-29 00:20:13 +0100

    Changed import style as per request.

--- minify.py
+++ minify.py
@@ -6,7 +6,7 @@ from os import walk
 from os.path import join
 import sys
 
-import htmlmin
+from htmlmin import minify as min
 from pelican import signals
 
 
@@ -43,9 +43,9 @@ def create_minified_file(filename):
     with open(filename, 'w', encoding='utf-8') as f:
         try:
             logger.debug('Minifying: %s' % filename)
-            compressed = htmlmin.minify(uncompressed, remove_comments=True,
-                                        remove_all_empty_space=True,
-                                        remove_empty_space=True)
+            compressed = min(uncompressed, remove_comments=True,
+                             remove_all_empty_space=True,
+                             remove_empty_space=True)
             f.write(compressed)
         except Exception as ex:
             logger.critical('HTML Minification failed: %s' % ex)
